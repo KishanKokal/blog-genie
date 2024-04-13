ad27af0df2aaed8a956f9e3e51fb8dec892dbfae
Kishan Kokal <kokalkishan.official@gmail.com>
2024-04-13 23:52:33 +0530

    Add script to generate embeddings

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# Steps to run ollama
-## Run the docker container
+# Steps to run the application
+
+## Install and run ollama
+
         docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+
 ## Install the llama2 model
+
         docker exec -it ollama ollama run llama2
+
+## Install and run mongodb community server
+
+        docker run -d -p 27017:27017 --name mongodb mongodb/mongodb-community-server
--- data-collection/scripts/generate-embeddings.js
+++ data-collection/scripts/generate-embeddings.js
@@ -1,20 +1,26 @@
 import { pipeline } from "@xenova/transformers";
 import fs from "node:fs";
 
-const posts = JSON.parse(fs.readFileSync("posts-with-content.json", "utf-8"))[
-  "posts"
-];
+const posts = JSON.parse(
+  fs.readFileSync("../data/posts-with-content.json", "utf-8")
+)["posts"];
 
 let generateEmbeddings = await pipeline(
   "feature-extraction",
   "Xenova/all-MiniLM-L6-v2"
 );
 
+let i = 0;
 for (const post of posts) {
+  console.log(i++);
   const result = await generateEmbeddings(post["content"], {
     pooling: "mean",
     normalize: true,
   });
-  console.log(post["content"]);
-  console.log(result);
+  post["content-embeddings"] = result;
 }
+
+fs.writeFileSync(
+  "../data/posts-with-embeddings.json",
+  JSON.stringify({ posts })
+);
